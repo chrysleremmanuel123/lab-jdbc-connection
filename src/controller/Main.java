abc22e88e4775af94bf8a4c4de7b32fbdc28644a
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-09-07 22:45:43 +0530

    lab day1

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,18 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
-		
-	// Fill your code
-		
+	public static void main(String args[]) throws ClassNotFoundException, SQLException {
+	Connection con;	
+	ConnectionManager cm = new ConnectionManager();
+	con=cm.getConnection();
+	if(con==null) {
+		System.out.println("Check your connection");
+	}
+	
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,31 @@
 package utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-
+import org.omg.CORBA.portable.InputStream;
 
 public class ConnectionManager {
-	
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		//step 1 Register the driver class
+				Class.forName("oracle.jdbc.OracleDriver");
+				
+				//step 2 Create the connection object to establish connection
+				Connection con=null;
+				con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","chryal_008");
+				
+				if(con!=null)
+					System.out.println("Established");
+				return con;
+	}
+	 public static Properties loadPropertiesFile() throws Exception {
+		 Properties prop = new Properties();
+		 InputStream in = (InputStream) ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		 prop.load(in);
+		 in.close();
+		 return prop; 
+		 } 
 	
 }
